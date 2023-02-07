01eb3eb2c8726c6016259d35b820bc261410c65a
jownew <JSarayno@aquip.io>
2023-02-08 01:06:25 +0800

    feat: add expense list page

--- database/factories/ExpenseFactory.php
+++ database/factories/ExpenseFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\PaymentType;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Expense>
@@ -16,8 +18,15 @@ class ExpenseFactory extends Factory
      */
     public function definition()
     {
+        $date = fake()->dateTimeBetween('-2 month', '+2 month');
         return [
-            //
+            'name' => fake()->name(),
+            'description' => fake()->name(),
+            'amount' => fake()->randomFloat(2),
+            'transaction_date' => $date,
+            'due_date' =>  $date,
+            'paid_date' =>  $date,
+            'payment_type_id' => PaymentType::all()->first()->id,
         ];
     }
 }
---
+++ resources/js/Pages/Expenses/Index.vue
@@ -0,0 +1,61 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import moment from "moment";
+import NavLink from '@/Components/NavLink.vue';
+
+const props = defineProps({
+    items: Object,
+});
+</script>
+
+<template>
+    <AppLayout title="Expenses">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Expenses
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="md:table w-full">
+                        <div class="md:table-header-group md:display hidden">
+                            <div class="table-row">
+                                <div class="table-cell text-left border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400"></div>
+                                <div class="table-cell text-left">Date</div>
+                                <div class="table-cell text-left">Name</div>
+                                <div class="table-cell text-right">Amount</div>
+                            </div>
+                        </div>
+                        <div class="md:table-row-group">
+                            <div
+                                v-for="item, i in items.data"
+                                :key="item.id"
+                                class="md:table-row odd:bg-white even:bg-gray-200 border py-2 my-2"
+                                >
+                                <div class="md:table-cell md:text-center hidden md:visible">
+                                    {{ i + 1 }}.
+                                </div>
+                                <div class="md:table-cell hidden md:visible">
+                                    {{ moment(item.transaction_date).format("DD MMM YYYY") }}
+                                </div>
+                                <div class="md:table-cell hidden md:visible">
+                                    {{ item.name }}
+                                </div>
+                                <div class="md:table-cell hidden md:visible text-right">
+                                    {{ item.amount.toLocaleString('en-US', {minimumFractionDigits:2, maximumFractionDigits:2}) }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center gap-1 text-gray-600 mt-4">
+                        <NavLink :key="key" v-for="(link, key) in items.links" :href="link.url" :active="link.active">
+                            <span v-html="link.label"></span>
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
