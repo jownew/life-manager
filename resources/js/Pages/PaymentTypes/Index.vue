b78efc5509cc83ce5122c2825a0c60d124131f1e
jownew <jownew@yahoo.com>
2023-02-11 01:02:19 +0800

    style: add buttons to payment type and currency pages

--- resources/js/Pages/PaymentTypes/Index.vue
+++ resources/js/Pages/PaymentTypes/Index.vue
@@ -1,9 +1,36 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+import { reactive } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
 
 const props = defineProps({
     items: Array,
 });
+
+const data = reactive({
+    'itemId': '',
+});
+
+const itemForm = useForm({
+    name: '',
+});
+
+const deleteItem = (id) => {
+    Inertia.delete(route('paymentTypes.destroy', id), {
+        preserveScroll: true,
+        onBefore: () => confirm('Are you sure you want to delete this item?')
+    });
+}
+
+const editItem = (id) => {
+    data.itemId = id;
+    console.log('edit item');
+}
+
 </script>
 
 <template>
@@ -17,25 +44,40 @@ const props = defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="md:text-right text-center mx-2">
+                        <PrimaryButton class="my-1" @click="editItem(0)" :class="{ 'opacity-25': itemForm.processing }"
+                            :disabled="itemForm.processing">
+                            New Payment Type
+                        </PrimaryButton>
+                    </div>
                     <div class="md:table w-full">
                         <div class="md:table-header-group md:display hidden">
                             <div class="table-row">
-                                <div class="table-cell text-left border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400"></div>
+                                <div
+                                    class="table-cell text-left border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                </div>
                                 <div class="table-cell text-left">Name</div>
                                 <div class="table-cell text-left"></div>
                             </div>
                         </div>
                         <div class="md:table-row-group">
-                            <div
-                                v-for="item, i in items"
-                                :key="item.id"
-                                class="md:table-row odd:bg-white even:bg-gray-200 border py-2 my-2"
-                                >
+                            <div v-for="item, i in items" :key="item.id"
+                                class="md:table-row odd:bg-white even:bg-gray-200 border py-2 my-2">
                                 <div class="md:table-cell md:text-center hidden md:visible">
                                     {{ i + 1 }}.
                                 </div>
                                 <div class="md:table-cell hidden md:visible">
                                     {{ item.name }}
+                                </div>
+                                <div class="md:table-cell hidden md:visible text-right">
+                                    <SecondaryButton class="mx-2 my-1" @click="editItem(item.id)"
+                                        :class="{ 'opacity-25': itemForm.processing }" :disabled="itemForm.processing">
+                                        Edit
+                                    </SecondaryButton>
+                                    <DangerButton class="mx-2 my-1" @click="deleteItem(item.id)"
+                                        :class="{ 'opacity-25': itemForm.processing }" :disabled="itemForm.processing">
+                                        Delete
+                                    </DangerButton>
                                 </div>
                             </div>
                         </div>
