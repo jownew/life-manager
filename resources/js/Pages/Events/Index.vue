9900e09631ff85e3821e2df952df27a7c66401ca
jownew <jownew@yahoo.com>
2023-04-19 14:32:41 +0800

    style: auto format

--- resources/js/Pages/Events/Index.vue
+++ resources/js/Pages/Events/Index.vue
@@ -1,77 +1,89 @@
 <template>
-    <AppLayout title="Events">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Events
-            </h2>
-        </template>
+  <AppLayout title="Events">
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Events
+      </h2>
+    </template>
 
-        <div class="md:py-3">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="md:text-right text-center mx-2">
-                        <PrimaryButton class="my-1" @click="editItem()" :class="{ 'opacity-25': itemForm.processing }"
-                            :disabled="itemForm.processing">
-                            New Event
-                        </PrimaryButton>
-                    </div>
-                    <div class="md:table w-full">
-                        <div class="md:table-header-group md:display hidden">
-                            <div class="table-row">
-                                <div
-                                    class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-right">
-                                </div>
-                                <div class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-left">Title</div>
-                                <div class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">Days Left</div>
-                                <div class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">Date</div>
-                                <div class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">Status</div>
-                                <div class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center"></div>
-                            </div>
-                        </div>
-                        <div class="md:table-row-group">
-                            <div v-for="item, i in items.data" :key="item.id"
-                                class="md:table-row odd:bg-white even:bg-gray-200 border py-2 my-2">
-                                <div class="md:table-cell md:text-center hidden md:visible">
-                                    {{ i + 1 }}.
-                                </div>
-                                <div class="md:table-cell hidden md:visible">
-                                    {{ item.title }}
-                                    <span class="bg-green-100 text-green-800 px-2.5 py-0.5 rounded-lg text-xs" v-if="item.is_owed === 0">I</span>
-                                    <span class="bg-red-100 text-red-800 px-2.5 py-0.5 rounded-lg text-xs" v-if="item.is_owed === 1">E</span>
-                                </div>
-                                <div class="md:table-cell hidden md:visible text-center">
-                                    {{  item.days }}
-                                </div>
-                                <div class="md:table-cell hidden md:visible text-center">
-                                    {{ moment(item.event_date).format("DD MMM YYYY") }}
-                                </div>
-                                <div class="md:table-cell hidden md:visible text-center">
-                                    {{ item.status }}
-                                </div>
-                                <div class="md:table-cell hidden md:visible text-right">
-                                    <SecondaryButton class="mx-2 my-1" @click="editItem(item.id)"
-                                        :class="{ 'opacity-25': itemForm.processing }" :disabled="itemForm.processing">
-                                        Edit
-                                    </SecondaryButton>
-                                    <DangerButton class="mx-2 my-1" @click="deleteItem(item.id)"
-                                        :class="{ 'opacity-25': itemForm.processing }" :disabled="itemForm.processing">
-                                        Delete
-                                    </DangerButton>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex justify-center gap-1 text-gray-600 mt-4">
-                        <NavLink :key="key" v-for="(link, key) in items.links" :href="link.url" :active="link.active">
-                            <span v-html="link.label"></span>
-                        </NavLink>
-                    </div>
+    <div class="md:py-3">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <div class="md:text-right text-center mx-2">
+            <PrimaryButton class="my-1" @click="editItem()" :class="{ 'opacity-25': itemForm.processing }"
+              :disabled="itemForm.processing">
+              New Event
+            </PrimaryButton>
+          </div>
+          <div class="md:table w-full">
+            <div class="md:table-header-group md:display hidden">
+              <div class="table-row">
+                <div
+                  class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-right">
                 </div>
+                <div
+                  class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-left">
+                  Title</div>
+                <div
+                  class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">
+                  Days Left</div>
+                <div
+                  class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">
+                  Date</div>
+                <div
+                  class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">
+                  Status</div>
+                <div
+                  class="table-cell border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-center">
+                </div>
+              </div>
             </div>
+            <div class="md:table-row-group">
+              <div v-for="item, i in items.data" :key="item.id"
+                class="md:table-row odd:bg-white even:bg-gray-200 border py-2 my-2">
+                <div class="md:table-cell md:text-center hidden md:visible">
+                  {{ i + 1 }}.
+                </div>
+                <div class="md:table-cell hidden md:visible">
+                  {{ item.title }}
+                  <span class="bg-green-100 text-green-800 px-2.5 py-0.5 rounded-lg text-xs"
+                    v-if="item.is_owed === 0">I</span>
+                  <span class="bg-red-100 text-red-800 px-2.5 py-0.5 rounded-lg text-xs"
+                    v-if="item.is_owed === 1">E</span>
+                </div>
+                <div class="md:table-cell hidden md:visible text-center">
+                  {{ item.days }}
+                </div>
+                <div class="md:table-cell hidden md:visible text-center">
+                  {{ moment(item.event_date).format("DD MMM YYYY") }}
+                </div>
+                <div class="md:table-cell hidden md:visible text-center">
+                  {{ item.status }}
+                </div>
+                <div class="md:table-cell hidden md:visible text-right">
+                  <SecondaryButton class="mx-2 my-1" @click="editItem(item.id)"
+                    :class="{ 'opacity-25': itemForm.processing }" :disabled="itemForm.processing">
+                    Edit
+                  </SecondaryButton>
+                  <DangerButton class="mx-2 my-1" @click="deleteItem(item.id)"
+                    :class="{ 'opacity-25': itemForm.processing }" :disabled="itemForm.processing">
+                    Delete
+                  </DangerButton>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center gap-1 text-gray-600 mt-4">
+            <NavLink :key="key" v-for="(link, key) in items.links" :href="link.url" :active="link.active">
+              <span v-html="link.label"></span>
+            </NavLink>
+          </div>
         </div>
-        <EventForm :isOpen="data.isEditModalOpen" :isReadOnly="data.isReadOnly" :itemId="data.itemId" :currencies="currencies"
-            @close="closeModal" @toReadOnly="toReadOnly" />
-    </AppLayout>
+      </div>
+    </div>
+    <EventForm :isOpen="data.isEditModalOpen" :isReadOnly="data.isReadOnly" :itemId="data.itemId" :currencies="currencies"
+      @close="closeModal" @toReadOnly="toReadOnly" />
+  </AppLayout>
 </template>
 
 <script setup>
@@ -87,43 +99,43 @@ import { reactive } from 'vue';
 import EventForm from './Partials/EventForm.vue';
 
 const props = defineProps({
-    items: Object,
-    currencies: Array,
+  items: Object,
+  currencies: Array,
 });
 
 const itemForm = useForm({
-    name: '',
-    description: '',
-    payment_type_id: '',
-    amount: null,
-    transaction_date: null,
+  name: '',
+  description: '',
+  payment_type_id: '',
+  amount: null,
+  transaction_date: null,
 });
 
 const data = reactive({
-    itemId: null,
-    isEditModalOpen: false,
-    isReadOnly: false,
+  itemId: null,
+  isEditModalOpen: false,
+  isReadOnly: false,
 });
 
 const editItem = (id = '') => {
-    data.itemId = id;
-    data.isEditModalOpen = true;
+  data.itemId = id;
+  data.isEditModalOpen = true;
 }
 
 const deleteItem = (id) => {
-    Inertia.delete(route('events.destroy', id), {
-        preserveScroll: true,
-        onBefore: () => confirm('Are you sure you want to delete this item?')
-    });
+  Inertia.delete(route('events.destroy', id), {
+    preserveScroll: true,
+    onBefore: () => confirm('Are you sure you want to delete this item?')
+  });
 };
 
 const closeModal = () => {
-    data.isEditModalOpen = false;
-    data.isReadOnly = false;
+  data.isEditModalOpen = false;
+  data.isReadOnly = false;
 }
 
 const toReadOnly = (value) => {
-    data.isReadOnly = value;
+  data.isReadOnly = value;
 }
 
 </script>
