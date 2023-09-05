cf02a562c3a651ea8b0eb1dea60c0f28b82dcee4
jownew <jownew@yahoo.com>
2023-09-05 11:10:37 +0800

    feat: add softdelete to events and expenses

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
 
     protected $fillable = [
         'user_id',
